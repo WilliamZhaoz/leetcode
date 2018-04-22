7372e60278572415ed313153d74fae8079650988
zhyzhao <zyzsilence@gmail.com>
2018-04-23 01:13:04 +0800

    206

---
+++ C++/206.reverse-linked-list.cpp
@@ -0,0 +1,59 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    // version 1 : 3 pointer iteratively, only reverse one link once move.
+    /*
+    ListNode* reverseList(ListNode* head) {
+        if (!head) return head;
+        if (!head->next) return head;
+        ListNode *pre = NULL, *cur = head, *nex = head->next;
+        while (nex) {
+            cur->next = pre;
+            pre = cur;
+            cur = nex;
+            nex = nex->next;
+        }
+        cur->next = pre;
+        return cur;
+    }
+    */
+    /*
+    // version 2 : insert every node after the head since the 2rd node recursively, finally move the head to the end.
+    ListNode* reverseList(ListNode* head) {
+        if (!head) return head;
+        if (!head->next) return head;
+        ListNode *pre = head->next, *cur = pre->next;
+        while (cur) {
+            pre->next = cur->next;
+            cur->next = head->next;
+            head->next = cur;
+            cur = pre->next;
+        }
+        ListNode * newHead = head->next;
+        pre->next = head;
+        head->next = NULL;
+        return newHead;
+    }
+    */
+    // version 3 : another version 2 implement with a dummy pointer before head.
+    ListNode* reverseList(ListNode* head) {
+        if(!head) return head;
+        if(!head->next) return head;
+        ListNode *pre = head, *cur = pre->next, *dummy_head = new ListNode(0);
+        dummy_head->next = head;
+        while (cur) {
+            pre->next = cur->next;
+            cur->next = dummy_head->next;
+            dummy_head->next = cur;
+            cur = pre->next;
+        }
+        return dummy_head->next;
+    }
+};
