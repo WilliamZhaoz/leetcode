66fb8ddeca6790178861c2ee55630e2c58aff091
zhyzhao <zyzsilence@gmail.com>
2018-05-03 00:54:12 +0800

    bug free 72

--- C++/72.edit-distance.cpp
+++ C++/72.edit-distance.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     int minDistance(string word1, string word2) {
-        if (word1.size() * word2.size() == 0) {
-            return word1.size() + word2.size();
-        }
         vector<vector<int>> dp(word1.size() + 1, vector<int>(word2.size() + 1, 0));
         for (int i = 0; i < word1.size() + 1; i++) {
             dp[i][0] = i;
@@ -16,7 +13,7 @@ public:
                 if (word1[i - 1] == word2[j - 1]) {
                     dp[i][j] = dp[i - 1][j - 1];
                 } else {
-                    dp[i][j] = min(min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+                    dp[i][j] = min(dp[i - 1][j], min(dp[i][j - 1], dp[i - 1][j - 1])) + 1;
                 }
             }
         }
