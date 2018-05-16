b90b7149039bed0e6ce166526e7e8acc182c9eb2
zhyzhao <zyzsilence@gmail.com>
2018-05-17 00:23:28 +0800

    update date and 145 solution

--- C++/145.binary-tree-postorder-traversal.cpp
+++ C++/145.binary-tree-postorder-traversal.cpp
@@ -29,6 +29,7 @@ public:
     */
     
     //non-recursive version
+    /*
     vector<int> postorderTraversal(TreeNode* root) {
         vector<int> res;
         stack<TreeNode*> s;
@@ -56,4 +57,29 @@ public:
         }
         return res;
     }
+    */
+    // non-recursive version 2
+    vector<int> postorderTraversal(TreeNode *root) {
+        vector<int> result;
+        stack<TreeNode *> myStack;
+        
+        TreeNode *current = root, *lastVisited = NULL;
+        while (current != NULL || !myStack.empty()) {
+            while (current != NULL) {
+                myStack.push(current);
+                current = current->left;
+            }
+            current = myStack.top(); 
+            if (current->right == NULL || current->right == lastVisited) {
+                myStack.pop();
+                result.push_back(current->val);
+                lastVisited = current;
+                current = NULL;
+            } else {
+                current = current->right;
+            }
+        }
+        return result;
+    }    
+    
 };
