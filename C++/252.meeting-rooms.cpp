6e5bbdb1921480be10d526ad1562b92eb603b751
v-zhzhao <v-zhzhao@microsoft.com>
2018-09-14 16:51:06 +0800

    update

---
+++ C++/252.meeting-rooms.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for an interval.
+ * struct Interval {
+ *     int start;
+ *     int end;
+ *     Interval() : start(0), end(0) {}
+ *     Interval(int s, int e) : start(s), end(e) {}
+ * };
+ */
+class Solution {
+public:
+    bool canAttendMeetings(vector<Interval>& intervals) {
+        if (intervals.empty() || intervals.size() == 1) {
+            return true;
+        }
+        sort(intervals.begin(), intervals.end(), [](const Interval &a, const Interval &b){return a.start < b.start;});
+        for (int i = 1; i < intervals.size(); i++) {
+            if (intervals[i].start < intervals[i - 1].end) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
