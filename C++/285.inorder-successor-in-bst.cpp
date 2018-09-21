ed4316a5d737bc67af227d2f50562efc565f0640
William <i@zhiyuanzhao.com>
2018-09-22 00:00:37 +0800

    update

--- C++/285.inorder-successor-in-bst.cpp
+++ C++/285.inorder-successor-in-bst.cpp
@@ -43,4 +43,59 @@ public:
         return res;
     }
 };
-// version 3: inorder traversal
+// version 3: recursive inorder traversal
+
+class Solution {
+public:
+    bool b = false, bb = false;
+    TreeNode *successor = NULL;
+    TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p) {
+        helper(root, p);
+        return successor;
+    }
+    void helper(TreeNode* root, TreeNode*p) {
+        if (!p || !root) {
+            return;
+        }
+        inorderSuccessor(root->left, p);
+        if (b && !bb) {
+            successor = root;
+            bb = true;
+        }
+        if (p == root) {
+            b = true;
+        }
+        inorderSuccessor(root->right, p);
+    }
+};
+
+// version 4: non-recursive inorder traversal
+
+class Solution {
+public:
+    TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p) {
+        if (!root || !p) {
+            return NULL;
+        }
+        stack<TreeNode*> s;
+        TreeNode *node = root;
+        bool b = false;
+        while (!s.empty() || node) {
+            if (node) {
+                s.push(node);
+                node = node->left;
+            } else {
+                if (b) {
+                    return s.top();
+                }
+                if (s.top() == p) {
+                    b = true;
+                }
+                node = s.top()->right;
+                s.pop();
+            }
+            
+        }
+        return NULL;
+    }
+};
